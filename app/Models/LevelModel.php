dcece521b509c1590aa83b221b8c978be81f59cc
Muhammad Khoirul Anwarudin <dimaskechiel58@gmail.com>
2025-03-11 00:48:43 +0700

    pertemuan week 4 ORM

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+    protected $table = 'm_level';
+    protected $primaryKey = 'level_id';
+    protected $fillable = ['level_kode', 'lavel_name'];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(UserModel::class);
+    }
+}
